bab52f365c12aa67c41079dd180e6783306fcaf7
hamzaidris <39967599+hamzaidris@users.noreply.github.com>
2025-08-20 11:47:26 +0300

    Update portfolio.py

--- trackerbazaar/portfolio.py
+++ trackerbazaar/portfolio.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from .tracker import PortfolioTracker
+from trackerbazaar.tracker import PortfolioTracker
 
 def render_portfolio(tracker):
     st.header("Portfolio Summary")
